0d8f4de0cfc8edb1b3ecbdad5bfe8aa973b95080
Diana <diana.ep.dev@gmail.com>
2024-09-16 20:26:32 +0000

    responsive done

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 export function Card () {
     return (
-<div className="card mb-4 p-3" style={{width: "18rem"}}>
+        <div className="row">
+        <div className="card col-3 mb-4 p-3" style={{width: "18rem"}}>
             <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJOPZxcr1N8MBo0EO-IVcU-OzQ3hpnb4Aokg&s" alt="Card image cap"/>
             <div className="card-body">
                 <h5 className="card-title">A very cute cat</h5>
                 <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non consequatur quae et aut consectetur sint eaque. Perspiciatis, optio. Hic molestias natus blanditiis, tempore quas voluptatum distinctio asperiores et cupiditate vel.</p>
                 <a href="#" className="btn btn-primary">Find Out More!</a>
             </div>
+        </div>
         </div>
     );
 }
